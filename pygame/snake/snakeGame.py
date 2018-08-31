f35e4083054c186f55ebd4f11ef0b07ed66afe1a
Carlyle <zachjohnsonsoftware@gmail.com>
2018-08-31 17:18:10 -0500

    draw body

--- pygame/snake/snakeGame.py
+++ pygame/snake/snakeGame.py
@@ -10,6 +10,8 @@ else:
 #initalize display
 gameDisplay = pygame.display.set_mode((720, 460))
 pygame.display.set_caption('Snake')
+x = 0
+y = 1
 
 fpsController = pygame.time.Clock()
 #colors
@@ -39,7 +41,67 @@ def gameOver():
     gameOverLanding.midtop = (360, 15)
     gameDisplay.blit(gameOverDisplay, gameOverLanding)
     pygame.display.flip()
+    time.sleep(5)
+    pygame.quit()
+    sys.exit()
 
-gameOver()
-time.sleep(10)
+#main
+
+while 1:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                changeDirection = 'RIGHT'
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                changeDirection = 'LEFT'
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                changeDirection = 'UP'
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                changeDirection = 'DOWN'
+            if event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+    #validate direction
+    if changeDirection == 'RIGHT' and not direction == 'LEFT':
+        direction = 'RIGHT'
+    if changeDirection == 'LEFT' and not direction == 'RIGHT':
+        direction = 'LEFT'
+    if changeDirection == 'UP' and not direction == 'DOWN':
+        direction = 'UP'
+    if changeDirection == 'DOWN' and not direction == 'UP':
+        direction = 'DOWN'
+    
+    #change direction
+    if direction == 'RIGHT':
+        snakePos[x] += 10
+    if direction == 'LEFT':
+        snakePos[x] -= 10
+    if direction == 'UP':
+        snakePos[y] -= 10
+    if direction == 'DOWN':
+        snakePos[y] += 10
+
+    #snake body
+    snakeBody.insert(x, list(snakePos))
+    if snakePos[x] == foodPos[x] and snakePos[y] == foodPos[y]:
+        foodSpawn = False
+    else:
+        snakeBody.pop()
+    if not foodSpawn:
+        foodPos = [random.randrange(1, 72)*10, random.randrange(1,46)*10]
+    foodSpawn = True
+
+    gameDisplay.fill(white)
+    for pos in snakeBody:
+        pygame.draw.rect(gameDisplay,green,pygame.Rect(pos[x],pos[y],10,10))
+    pygame.draw.rect(gameDisplay,red,pygame.Rect(foodPos[x],foodPos[y],10,10)) 
+    pygame.display.flip()
+    fpsController.tick(25)
+
+
+
 
