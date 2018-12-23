826e096ef7b8049b4f12500b71307e749de261a4
MersenneInteger <zachjohnsonsoftware@gmail.com>
2018-12-22 19:18:45 -0500

    Basic calculator exercise

---
+++ C#/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public double Calculate(double op1, double op2, string operation)
+        {
+            switch (operation)
+            {
+                case ("+"):
+                    return op1 + op2;
+                case ("-"):
+                    return op1 - op2;
+                case ("*"):
+                    return op1 * op2;
+                case ("/"):
+                    try
+                    {
+                        return op1 / op2;
+                    }
+                    catch
+                    {
+                        throw new DivideByZeroException();
+                    }
+                default:
+                    throw new InvalidOperationException("Operation is invalid");
+            }
+        }
+    }
+}
---
+++ C#/Calculator/Calculator/InputConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class InputConverter
+    {
+        public double ConvertInputToNum(string argTextInput)
+        {
+            double convertedNum;
+            if (!double.TryParse(argTextInput, out convertedNum))
+                throw new ArgumentException("Expected a numeric value");
+            return convertedNum;
+
+        }
+    }
+}
