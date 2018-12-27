77c9b0583f3455af419f86b8299f1a9f7ebbc8cf
MersenneInteger <zachjohnsonsoftware@gmail.com>
2018-12-26 19:15:32 -0500

    Add files via upload

---
+++ C#/Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public double Calculate(double op1, double op2, string operation)
+        {
+            switch (operation)
+            {
+                case ("+"):
+                    return op1 + op2;
+                case ("-"):
+                    return op1 - op2;
+                case ("*"):
+                    return op1 * op2;
+                case ("/"):
+                    try
+                    {
+                        return op1 / op2;
+                    }
+                    catch
+                    {
+                        throw new DivideByZeroException();
+                    }
+                case ("^"):
+                    checked
+                    {
+                        return Math.Pow(op1, op2);
+                    }
+                default:
+                    throw new InvalidOperationException("Operation is invalid");
+            }
+        }
+    }
+}
---
+++ C#/Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void parseInput(ref double targetVar)
+        {
+            if (!double.TryParse(Console.ReadLine(), out targetVar))
+                throw new ArgumentException("Expected a numeric value");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    Calculator calculator = new Calculator();
+
+                    double operand1 = 0, operand2 = 0;
+                    parseInput(ref operand1);
+                    string operation = Console.ReadLine();
+                    parseInput(ref operand2);
+                    Console.WriteLine("= " + calculator.Calculate(operand1, operand2, operation) + "\n");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
